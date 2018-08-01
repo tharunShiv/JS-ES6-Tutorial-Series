21054db8be88de342e62d4ca56b7906fb49f8593
tharunShiv <tharun.rocky14@gmail.com>
2018-08-01 20:28:19 +0530

    JS2

---
+++ JS2.js
@@ -0,0 +1,9 @@
+// Variables
+// var Den = "Lion Roars";
+// console.log("Tharun Said " + Den);
+
+// let Den = 5;
+// console.log("Tharun has " + Den + " PCs");
+
+// const Den = 5;
+// console.log("Tharun has " + Den + " PCs");
