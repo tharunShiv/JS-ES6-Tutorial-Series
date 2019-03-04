44ed77a2e610e4489dd548857d68f48a7d1d99f8
tharunShiv <tharun.rocky14@gmail.com>
2019-03-04 17:50:03 +0530

    looping statements

---
+++ JS9.js
@@ -0,0 +1,58 @@
+// Looping statements
+
+// definite looping
+
+// FOR LOOP
+
+var x = 4
+var factorial = 1;
+
+for (let i = x; i >= 1; i--) {
+    if (i == 3) {
+        continue;
+    }
+    factorial *= i
+
+}
+
+console.log(factorial)
+
+// // FOR IN
+// var object = { 'name': 'shiv', 'age': '20' }
+
+// for (let prop in object) {
+//     console.log(object[prop])
+// }
+
+// // for of
+// for (let item of [10, 12, 14]) {
+//     console.log(item)
+// }
+
+
+// // indefinite loopings
+
+
+// // while loop
+// var y = 5
+// factorial = 1
+
+// while (y >= 1) {
+//     factorial *= y
+//     y--
+// }
+
+// console.log(factorial)
+
+// // do while loop
+
+// var y = 5
+// factorial = 1
+// do {
+//     factorial *= y
+//     y--
+// } while (y >= 1)
+
+// console.log(factorial)
+
+
