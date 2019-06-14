f19b74fe70f213be0af04c480313a5547f3da60c
tharunShiv <tharun.rocky14@gmail.com>
2019-06-14 22:20:46 +0530

    functions-2 complete

--- JS10.js
+++ JS10.js
@@ -36,3 +36,53 @@ function lengthOfMe(...params) {
 }
 
 console.log(lengthOfMe(5, 6, 7, 8));
+
+// function constructor
+let func = new Function("x", "y", "return x*y;");
+
+console.log(func(5, 6));
+
+// 6 = 6x5x4x3x2x1
+function factorial(num) {
+  if (num <= 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+console.log(factorial(6));
+
+// anonymous function
+(function() {
+  console.log("I'm an anonymous");
+})();
+
+// parameters
+// fat arrow notation
+// statements
+var food = (x, y, z) => {
+  return x + y + z;
+};
+
+console.log("food: ", food(10, 20, 30));
+
+// variation 1
+var food = (x, y, z) => x + y + z;
+console.log("food: ", food(10, 20, 30));
+
+// variation 2
+var food = x => x + 100;
+console.log("food: ", food(10));
+
+// generator functions
+function* query() {
+  const name = yield "What's your name?";
+  const sport = yield "What's your favourite sport? ";
+  return `${name}'s fav sport is ${sport}`;
+}
+
+const instance = query();
+console.log(instance.next());
+console.log(instance.next("Shiv"));
+console.log(instance.next("Basketball"));
