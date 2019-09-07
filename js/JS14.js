1f49cfd2f70d62229bb4e6499ff474b8864578c8
tharunShiv <tharun.rocky14@gmail.com>
2019-09-07 17:32:48 +0530

    object constructor

--- js/JS14.js
+++ js/JS14.js
@@ -1,1 +1,22 @@
 // JS Objects
+
+// JS Object Constructor
+
+let myLaptop = new Object();
+let propertyName = "resolution";
+
+myLaptop.brand = "Dell";
+myLaptop.cores = "8";
+myLaptop.storage = "1TB";
+myLaptop[propertyName] = "1080p";
+
+console.log(
+  "My Laptop Info: ",
+  myLaptop.brand,
+  " Cores: ",
+  myLaptop["cores"],
+  ", Storage: ",
+  myLaptop.storage,
+  "Screen Resolution",
+  myLaptop.resolution
+);
